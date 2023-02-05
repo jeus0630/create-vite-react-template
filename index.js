de1fb9535610cddec894a1726ae01ff9e19df8d0
Jewoo <jeus0630@gmail.com>
2023-02-06 00:10:00 +0900

    Update package.json name with user's projectName

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function (o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     var desc = Object.getOwnPropertyDescriptor(m, k);
     if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
+        desc = { enumerable: true, get: function () { return m[k]; } };
     }
     Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
+}) : (function (o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
 }));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function (o, v) {
     Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
+}) : function (o, v) {
     o["default"] = v;
 });
 var __importStar = (this && this.__importStar) || function (mod) {
@@ -93,12 +93,15 @@ function createDirectoryContents(templatePath, projectName) {
         if (SKIP_FILES.indexOf(file) > -1)
             return;
         if (stats.isFile()) {
-            
+
             // read file content and transform it using template engine
             let contents = fs.readFileSync(origFilePath, "utf8");
             // contents = template.render(contents, { projectName });
             // write file to destination folder
             const writePath = path.join(CURR_DIR, projectName, file === "_gitignore" ? ".gitignore" : file);
+            if (file === "package.json") {
+                contents = contents.replace("react-template", projectName);
+            }
             fs.writeFileSync(writePath, contents, "utf8");
         }
         else if (stats.isDirectory()) {
@@ -121,7 +124,7 @@ function postProcess(options) {
             console.log(chalk_1.default.yellow(`  yarn`));
             console.log(chalk_1.default.dim("\nRun app:"));
             console.log(chalk_1.default.yellow(`  yarn dev`));
-            console.log(chalk_1.default.green("\nEnjoy!\n"));    
+            console.log(chalk_1.default.green("\nEnjoy!\n"));
         })
     }
     return true;
